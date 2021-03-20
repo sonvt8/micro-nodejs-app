de26a19ded713a28bd25e3b03d03326c18200e4d
Thai Son Vu <vuthaison198x@gmail.com>
2021-03-20 16:33:26 +0700

    fetching data in the React app

---
+++ client/src/Fib.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import anxios from 'anxios';
+
+class Fib extends Component {
+    state = {
+        seenIndexes: [],
+        values: {},
+        index: ''
+    };
+
+    componentDidMount(){
+        this.fetchValues();
+        this.fetchIndexes();
+    }
+};
