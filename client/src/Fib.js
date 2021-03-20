b7827eb17e21b4a8291eed04ddcfc16d3a4c6b7f
Thai Son Vu <vuthaison198x@gmail.com>
2021-03-20 16:39:02 +0700

    fix error using wrong Http Client

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import anxios from 'anxios';
+import axios from 'axios';
 
 class Fib extends Component {
     state = {
@@ -11,5 +11,17 @@ class Fib extends Component {
     componentDidMount(){
         this.fetchValues();
         this.fetchIndexes();
+    }
+
+    async fectchValues() {
+        const values = await axios.get('/api/values/current');
+        this.setState({values: values.data});
+    }
+
+    async fetchIndexes(){
+        const seenIndexes = await axios.get('/api/values/all');
+        this.setState({
+            seenIndexes: seenIndexes.data
+        });
     }
 };
